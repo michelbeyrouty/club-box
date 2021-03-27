51c14ccaddd33003ac4fbd301687e197607c3501
michel.beyrouty@washmen.com <Michel El Beyrouty>
2021-03-27 09:29:05 +0200

    [#1] Add socket and webRTC setup draft 1

--- index.js
+++ index.js
@@ -15,3 +15,47 @@ const server = http.createServer(app);
 server.listen(process.env.PORT || 3000, () =>{
   console.log(`Listening on port -> ${server.address().port}`);
 });
+
+// Connection event is fired as soon as a client connects to this socket.
+
+const io = socketIO(server);
+io.sockets.on('connection', onSocketConnection);
+
+function onSocketConnection (socket) {
+
+  socket.on('message', (message, room) => {
+	  console.log('Client said: ', message);
+	  socket.in(room).emit('message', message, room);
+  });
+
+  socket.on('create or join', (room) => {
+
+    const clientsInRoom = io.sockets.adapter.rooms[room];
+    const numClients = clientsInRoom ? Object.keys(clientsInRoom).length : 0;
+
+    console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    switch (numClients) {
+      case 0:
+        socket.join(room);
+        console.log('Client ID ' + socket.id + ' created room ' + room);
+        socket.emit('created', room, socket.id);
+        break;
+      case 1:
+        console.log('Client ID ' + socket.id + ' joined room ' + room);
+        io.sockets.in(room).emit('join', room);
+        socket.join(room);
+        socket.emit('joined', room, socket.id);
+        io.sockets.in(room).emit('ready');
+        break;
+      default:
+        socket.emit('full', room);
+	  }
+
+  });
+
+  socket.on('bye', ()=>  {
+	  console.console.log('received bye');
+  });
+
+}
